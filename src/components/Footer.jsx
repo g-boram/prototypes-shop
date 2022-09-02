7fa0b53f1659bb84e61524d2b6f40e95265075a4
g-boram <go_bo_ram@naver.com>
2022-09-02 15:12:59 +0900

    prototypes-shop

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+// components/Footer.jsx
+
+export default function Footer() {
+  return (
+    <footer>
+      <p>© 2022 Boram Go.</p>
+    </footer>
+  );
+}
+
---
+++ src/hooks/usePrototypes.js
@@ -0,0 +1,11 @@
+// hooks/usePrototypes.js
+
+import { useContext } from "react";
+import AppStateContext from "../contexts/AppStateContext";
+
+export default function usePrototypes() {
+  const { prototypes } = useContext(AppStateContext);
+
+  return prototypes;
+}
+
